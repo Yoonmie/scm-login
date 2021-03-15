f145970903d665629923c2404bd5fce86f742208
Yoonmie <scm.yoonmiemieaung@gmail.com>
2021-03-15 16:35:41 +0630

    added

--- connect.php
+++ connect.php
@@ -5,7 +5,4 @@ $conn=mysqli_select_db($db,'blog');
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-else{
-  echo "Connected successfully";
-}
 ?>
--- update.php
+++ update.php
@@ -5,7 +5,7 @@ $name=$_POST['name'];
 $email=$_POST['email'];
 $pw=$_POST['password'];
 echo $id,$name,$email,$pw;
-$update="UPDATE customer SET customername='$name',email='$email',password='$pw' WHERE customerid=$id";
+$update="UPDATE users SET name='$name',email='$email',password='$pw' WHERE id=$id";
 mysqli_query($db,$update);
 header("Location:reg.php?status=2");
 ?>
